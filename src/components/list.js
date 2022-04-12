9e847060a5723ce092998d2111337a25b0db9bc1
Your Name <samsulr99@gmail.com>
2022-04-12 11:06:44 +0700

    update

--- src/components/list.js
+++ src/components/list.js
@@ -1,1 +1,13 @@
 // code Here
+import React from 'react'
+
+function List(props) {
+    return (
+        <div>            
+            <p>{props.name}</p>
+            <p>{props.price}</p>
+        </div>
+    )
+}
+
+export default List
--- src/props.js
+++ src/props.js
@@ -1,11 +1,36 @@
 import React from "react";
+import List from "./components/list";
 
 //Import Components
 
+const data = [
+  {
+    name: 'Product 1',
+    price: 20000
+  },
+  {
+    name: 'Product 2',
+    price: 20000
+  },
+  {
+    name: 'Product 3',
+    price: 20000
+  },
+]
+
 function Props() {
+
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      {data.map((item) => {
+        return(
+          <div>
+            <List name={item.name} price={item.price} />
+          </div>
+        )
+      })}
+    </div>
   );
 }
 
