3a0e970c32f8aa8824a0468a86b99d86e25e4a83
Your Name <samsulr99@gmail.com>
2022-04-12 10:39:16 +0700

    update

--- src/embedExpression.js
+++ src/embedExpression.js
@@ -2,13 +2,25 @@ import React from "react";
 
 function EmbedExpression() {
   //Create function here
+  const getBatch = (a, b) => {
+      const jumlah = a + b
+
+      return jumlah
+    
+  } 
 
   //Create a variable here
+  const name = 'Samsul Rijal'
 
   return (
     // Code Inside div
-    <div></div>
+    <div>
+      <h2>{name}</h2>
+      <p>{getBatch(10,20)}</p>
+    </div>
   );
 }
+
+
 
 export default EmbedExpression;
